9dd2b6fb06a09b37b3f31a80af88c6c6b76f2a53
Kalpak Goshikwar <kalpakgoshikwar123@gmail.com>
2024-08-02 14:21:12 +0530

    UPDATE: About section

--- src/components/LandingPage/About.tsx
+++ src/components/LandingPage/About.tsx
@@ -7,18 +7,18 @@ import { ImQuotesLeft } from "react-icons/im";
 export default function About() {
   const t = useTranslations("main.about");
   return (
-    <section className="px-8 xl:px-40 pt-10 py-20 space-y-8 text-justify">
+    <section className="md:px-8 xl:px-40 pt-10 py-20 space-y-8 text-justify">
       <div className=" grid grid-cols-1 md:grid-cols-2 gap-8 place-items-center">
         <div data-aos="fade-in">
           <div className="pt-6 md:pb-20 md:px-8 relative">
             <Image
-              src={"/assets/LandingPage/narendra-profile.png"}
+              src={"/assets/About/Amadar.jpg"}
               alt="Narendra Profile"
               width={500}
               height={500}
-              className="rounded-t-xl rounded-b-0 md:rounded-b-xl border-8 border-[var(--primary-clr)]"
+              className="md:max-w-[500px] md:max-h-[600px] object-cover object-top md:rounded-xl"
             />
-            <div className="p-4 bg-[var(--primary-clr)] w-fit md:max-w-[60%] md:absolute bottom-0 left-0  rounded-t-0 md:rounded-t-xl rounded-b-xl">
+            <div className="p-4 bg-[var(--primary-clr)] mt-4 w-fit md:max-w-[60%] md:absolute bottom-0 left-0  rounded-t-0 md:rounded-t-xl md:rounded-b-xl">
               <ImQuotesLeft size={28} className="text-black" />
               <div className="pl-8">
                 <p className="font-[600] text-white italic">{t("mission")}</p>
@@ -29,7 +29,7 @@ export default function About() {
             </div>
           </div>
         </div>
-        <div className="space-y-8"  data-aos="fade-left">
+        <div className="px-8 md:px-0 space-y-8"  data-aos="fade-left">
           <h2>{t("title")}</h2>
           <div className="space-y-4">
             <p>{t("para1")}</p>
